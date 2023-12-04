1e87765d5ba47240f5ada166a769a9df3b35a507
Alejandro Ruzafa <ruzafa8@gmail.com>
2023-12-04 18:26:49 +0100

    fix: validation numbers RGB

--- include/errors.h
+++ include/errors.h
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:48:31 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/12/04 16:20:14 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/12/04 18:03:53 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 # define NO_PLAYER_ERR "Error\nMap has got no player"
 # define TOO_MUCH_PLAYERS_ERR "Error\nMap has got too much players"
 # define UNKNOWN_CHARACTER_MAP_ERR "Error\nMap has got an unknown character"
-# define INVALID_RGB_COLOR_ERR "Error\nRGB number is not valid"
+# define RGB_TOO_LONG_ERR "Error\nRGB value is so_long"
+# define RGB_NAN_ERR "ERROR\nRGB value is not a number"
+# define RGB_NUM_PARAMS_ERR "ERROR\nRGB number of params error"
 
 typedef enum e_error
 {
@@ -32,7 +34,9 @@ typedef enum e_error
 	NO_PLAYER,
 	TOO_MUCH_PLAYERS,
 	UNKNOWN_CHARACTER_MAP,
-	INVALID_RGB_COLOR,
+	RGB_TOO_LONG,
+	RGB_NAN,
+	RGB_NUM_PARAMS,
 }	t_error;
 
 #endif
--- src/validator/validate_colors.c
+++ src/validator/validate_colors.c
@@ -6,52 +6,26 @@
 /*   By: aruzafa- <aruzafa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:37:03 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/12/04 16:23:20 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/12/04 18:25:53 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/**
- * This function validates that the string received is number between 0 and 255.
- * 
- * Firstable, we validate len (must not be bigger than 3). That way we avoid
- * overflows.
- * 
- * Then we validate each char of the string is a number.
- */
-static int	validate_number(char *number)
+static size_t	count_num_strings(char *numbers)
 {
-	int	i;
+	size_t	num_commas;
+	size_t	i;
 
-	if (ft_strlen(number) > 3)
-		return (0);
 	i = 0;
-	while (number[i])
+	num_commas = 0;
+	while (numbers[i])
 	{
-		if (!ft_isdigit((int) number[i]))
-			return (0);
+		if (numbers[i] == ',')
+			num_commas++;
 		i++;
 	}
-	return (1);
-}
-
-static size_t	count_num_strings(char **strs)
-{
-	size_t	num;
-
-	num = 0;
-	while (*strs)
-	{
-		num++;
-		strs++;
-	}
-	return (num);
-}
-
-static int	valid_range(int number)
-{
-	return (0 <= number && number <= 255);
+	return (num_commas);
 }
 
 static int	rgb_to_int_color(int r, int g, int b)
@@ -59,39 +33,74 @@ static int	rgb_to_int_color(int r, int g, int b)
 	return (r << 24 | g << 16 | b << 8 | 0xff);
 }
 
-int	validate_colors(char *rgb, t_error *error)
+/**
+ * This function validates that the string received is number between 0 and 255.
+ * 
+ * We validate for each char if it a number.
+ * Then, we validate the len (must not be bigger than 3). That way we avoid
+ * overflows.
+ * 
+ * Finally we convert it to int and validate range between 0 and 255
+ */
+static t_error	validate_number(char *number, int *number_to_int)
 {
-	char	**numbers;
-	int		r_number;
-	int		g_number;
-	int		b_number;
+	int	i;
 
-	numbers = ft_split(rgb, ',');
-	if (!numbers)
+	i = 0;
+	if (!number)
+		return (RGB_NUM_PARAMS);
+	while (number[i])
 	{
-		*error = MEMORY_ERROR;
-		return (0);
+		if (!ft_isdigit((int) number[i]))
+			return (RGB_NAN);
+		i++;
 	}
-	if (count_num_strings(numbers) != 3)
+	if (ft_strlen(number) > 3)
+		return (RGB_TOO_LONG);
+	*number_to_int = ft_atoi(number);
+	if (*number_to_int < 0 || *number_to_int > 255)
+		return (RGB_TOO_LONG);
+	return (NO_ERROR);
+}
+
+static void free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i] != 0)
 	{
-		*error = INVALID_RGB_COLOR;
-		return (0);
+		free(split[i]);
+		i++;
 	}
-	if (!validate_number(numbers[0]) || !validate_number(numbers[1])
-		|| !validate_number(numbers[2]))
-	{
-		*error = INVALID_RGB_COLOR;
-		return (0);
-	}
-	r_number = ft_atoi(numbers[0]);
-	g_number = ft_atoi(numbers[1]);
-	b_number = ft_atoi(numbers[2]);
-	if (!valid_range(r_number) || !valid_range(g_number)
-		|| !valid_range(b_number))
-	{
-		*error = INVALID_RGB_COLOR;
-		return (0);
-	}
-	*error = NO_ERROR;
-	return (rgb_to_int_color(r_number, g_number, b_number));
+	free(split);
+}
+
+/**
+ * TODO: Revisar bien los Leaks... 
+ * numbers_str es un array de arrays, no lo estamos borrando bien
+ */
+t_error	validate_colors(char *rgb, int *number)
+{
+	char	**numbers_str;
+	int		*numbers;
+	t_error	error;
+
+	if (count_num_strings(rgb) != 2)
+		return (RGB_NUM_PARAMS);
+	numbers_str = ft_split(rgb, ',');
+	numbers = (int *) ft_calloc(3, sizeof(int));
+	if (!numbers_str || !numbers)
+		return (free(numbers), free_split(numbers_str), MEMORY_ERROR);
+	error = validate_number(numbers_str[0], numbers);
+	if (error != NO_ERROR)
+		return (free(numbers), free_split(numbers_str), error);
+	error = validate_number(numbers_str[1], numbers + 1);
+	if (error != NO_ERROR)
+		return (free(numbers), free_split(numbers_str), error);
+	error = validate_number(numbers_str[2], numbers + 2);
+	if (error != NO_ERROR)
+		return (free(numbers), free_split(numbers_str), error);
+	*number = rgb_to_int_color(numbers[0], numbers[1], numbers[2]);
+	return (free(numbers), free_split(numbers_str), NO_ERROR);
 }
