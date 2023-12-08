54e46136c6ec10a427e2726186c85731d82213d3
Alejandro Ruzafa <ruzafa8@gmail.com>
2023-12-08 19:51:27 +0100

    fix: validate textures

--- src/init_data.c
+++ src/init_data.c
@@ -6,33 +6,51 @@
 /*   By: aruzafa- <aruzafa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 19:01:13 by atrujill          #+#    #+#             */
-/*   Updated: 2023/12/08 18:09:48 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/12/08 19:31:17 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static mlx_texture_t	*load_png(char *file)
+{
+	int		fd;
+	size_t	filename_len;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	close(fd);
+	filename_len = ft_strlen(file);
+	if (filename_len > 4 && file[filename_len - 4] == '.'
+		&& file[filename_len - 3] == 'p'
+		&& file[filename_len - 2] == 'n'
+		&& file[filename_len - 1] == 'g')
+		return (mlx_load_png(file));
+	return (0);
+}
+
 void	init_textures(t_data *data)
 {
-	data->textures[NORTH] = mlx_load_png(data->cub3d->north_texture);
+	data->textures[NORTH] = load_png(data->cub3d->north_texture);
 	if (!data->textures[NORTH])
 	{
 		ft_putendl_fd(LOAD_ERR, STDERR_FILENO);
 		free_and_exit(data, EXIT_SUCCESS);
 	}
-	data->textures[SOUTH] = mlx_load_png(data->cub3d->south_texture);
+	data->textures[SOUTH] = load_png(data->cub3d->south_texture);
 	if (!data->textures[SOUTH])
 	{
 		ft_putendl_fd(LOAD_ERR, STDERR_FILENO);
 		free_and_exit(data, EXIT_SUCCESS);
 	}
-	data->textures[EAST] = mlx_load_png(data->cub3d->east_texture);
+	data->textures[EAST] = load_png(data->cub3d->east_texture);
 	if (!data->textures[EAST])
 	{
 		ft_putendl_fd(LOAD_ERR, STDERR_FILENO);
 		free_and_exit(data, EXIT_SUCCESS);
 	}
-	data->textures[WEST] = mlx_load_png(data->cub3d->west_texture);
+	data->textures[WEST] = load_png(data->cub3d->west_texture);
 	if (!data->textures[WEST])
 	{
 		ft_putendl_fd(LOAD_ERR, STDERR_FILENO);
